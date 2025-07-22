51cf9a2fd4fa73f30ec3aa302691d202b095bb9e
edenWerke <edenw6292@gmail.com>
2025-07-22 16:17:08 +0300

    creating colors

---
+++ lib/models/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color correct=Color.fromARGB(255, 16, 239, 157);
+
+const Color incorrect=Color.fromARGB(255, 242, 64, 29);
+
+const Color neutral=Color.fromARGB(217, 255, 255, 255);
+
+const Color background=Color.fromARGB(255, 151, 86, 207);
